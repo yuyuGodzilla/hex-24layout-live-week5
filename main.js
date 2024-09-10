3a71320dfbad4f7fbd42e596dc4e138cae52780a
yuyuGodzilla <godnochi@gmail.com>
2024-09-11 01:22:59 +0800

    first commit

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
-import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
 
 console.log("Hello world!");
+
+// 以下預設開啟 Modal用程式碼
+// import {Modal} from 'bootstrap';
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     var myModal = new Modal(document.getElementById('viewModal'), {
+//       backdrop: 'static', // 選擇是否點擊背景時關閉
+//       keyboard: false     // 選擇是否允許按下鍵盤上的 "ESC" 鍵關閉 Modal
+//     });
+//     myModal.show(); // 自動顯示 Modal
+//   });
